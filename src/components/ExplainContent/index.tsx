a23f41cb8299a2355ffdaa045da411eb0b057ef9
chujotakaki <takakiking@yahoo.co.jp>
2023-12-07 17:07:31 +0900

    ExplainContentsコンポーネント作成、それに伴うcss修正

--- src/components/ExplainContent/index.tsx
+++ src/components/ExplainContent/index.tsx
@@ -4,19 +4,34 @@ type Props = {
   title: string;
   span: string;
   info: ExplainContentInfo[];
-  content?: string;
+  content: string;
+  anotation: string;
 };
-export const ExplainContent: FC<Props> = ({ title, span, info, content }) => {
+export const ExplainContent: FC<Props> = ({
+  title,
+  span,
+  info,
+  content,
+  anotation,
+}) => {
   return (
-    <div className='pr-10 pb-10 pl-10'>
-      <h2>{title}</h2>
-      <span>{span}</span>
+    <div className='pr-10 pb-3 pl-10 pt-6 w-480 text-center'>
+      <h2 className='text-23 pb-6'>
+        {title}
+        <span className='text-18'>{span}</span>
+      </h2>
+
       <ul>
         {info.map((item) => {
-          return <li key={item.content}>{item.content}</li>;
+          return (
+            <li className='text-10' key={item.content}>
+              {`・${item.content}`}
+            </li>
+          );
         })}
       </ul>
       <p>{`＜贈品＞${content}`}</p>
+      <p>{anotation}</p>
     </div>
   );
 };
