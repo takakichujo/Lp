de73e16e177adee368bf5a6e82f6430f1f1ff8ba
chujotakaki <takakiking@yahoo.co.jp>
2023-12-11 10:11:05 +0900

    Routing適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './index.css';
-import { Contents } from './components/Contents';
-import { ContentInfoTop } from './lib/const/ContentsInfoTop';
-import { Button } from './components/Button/Button';
+import { useNavigate } from 'react-router-dom';
 function App() {
+  const navigate = useNavigate();
+  const handleCampain = () => {
+    navigate('/Campain');
+  };
   return (
-    <>
-      <Contents contentsInfoArray={ContentInfoTop} />
-      <Button />
-    </>
+    <div className='text-center'>
+      <h1 className='text-23'>ページ遷移</h1>
+      <p className='text-18' onClick={handleCampain}>
+        キャンペーンページへ
+      </p>
+    </div>
   );
 }
 
