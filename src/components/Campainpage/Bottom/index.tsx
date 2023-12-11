e3fd08cc6b74357f5e0b801f3a6511ff7731ea8e
chujotakaki <takakiking@yahoo.co.jp>
2023-12-11 23:21:00 +0900

    Bottomコンテンツリファクタ、レスポンシブ対応

--- src/components/Campainpage/Bottom/index.tsx
+++ src/components/Campainpage/Bottom/index.tsx
@@ -6,13 +6,13 @@ import { ContentInfoBottom } from '../../../lib/const/ContentsInfoBottom';
 export const Bottom: FC = () => {
   return (
     <div className='lg:w-986 w-full lg:mt-50 mt-20 lg:mr-30 lg:ml-30 bg-bgBlue bg-cover bg-center pb-10 text-center lg:pt-10'>
-      <ExplainBottomContentsDetail className='lg:m-4' />
+      <ExplainBottomContentsDetail className='lg:m-4 ' />
       <UnionButton
         className='text-center'
         message='10,800'
         content='原價 11,088日圓(含稅)，現省1,008日圓'
       />
-      <Contents contentsInfoArray={ContentInfoBottom} className='mt-40' />
+      <Contents contentsInfoArray={ContentInfoBottom} />
     </div>
   );
 };
--- src/components/Campainpage/ExplainBottomContents/index.tsx
+++ src/components/Campainpage/ExplainBottomContents/index.tsx
@@ -7,22 +7,24 @@ type Props = {
 };
 export const ExplainBottomContentsDetail: FC<Props> = ({ className }) => {
   return (
-    <div className={`flex lg:pt-50 pl-15 lg:flex-row flex-col ${className}`}>
+    <div
+      className={`flex lg:pt-50 pl-15 lg:flex-row lg:h-430 flex-col ${className}`}
+    >
+      <div className='relative h-auto flex flex-col'>
+        <img
+          src='../../src/img/CampainImg/fancl_bottom_content.webp'
+          className='lg:w-450 lg:ml-4  w-90% mx-auto mb-4 lg:mb-0 h-auto object-contain lg:order-1 order-2'
+        />
+        <Baloon className='absolute lg:-top-full left-1/2 top-10 lg:order-2 order-1' />
+      </div>
       <ExplainContent
         title='毛孔護理套組'
         span=''
         content='MCO速淨卸粧油 60ml'
         info={ExplainContentsBottomInfo}
         anotation=''
-        className='order-2 lg:order-1'
+        className='h-300'
       />
-      <div className='relative lg:h-400 h-auto order-1 lg:order-2 flex flex-col'>
-        <img
-          src='../../src/img/CampainImg/fancl_bottom_content.webp'
-          className='lg:w-450 ml-4 lg:h-400 max-w-full h-auto object-contain lg:order-1 order-2'
-        />
-        <Baloon className='absolute lg:-top-full left-1/2 top-10  lg:order-2 order-1' />
-      </div>
     </div>
   );
 };
